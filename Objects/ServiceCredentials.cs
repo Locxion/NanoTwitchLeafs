2e1ddffa49bc20fafd7a5ad30d98a9ab20d74a93
Locxion <locxion.m@googlemail.com>
2023-02-24 02:02:30 +0100

    Added ServiceCredential Class

---
+++ Objects/ServiceCredentials.cs
@@ -0,0 +1,29 @@
+﻿namespace NanoTwitchLeafs.Objects
+{
+	/// <summary>
+	/// Class to Store all kinds of Credentials with a ClientId & Secret pair
+	/// </summary>
+	public class ServiceCredentials
+	{
+		public TwitchApiCredentials TwitchApiCredentials { get; set; }
+		public StreamLabsApiCedentials StreamLabsApiCedentials { get; set; }
+	}
+
+	/// <summary>
+	/// Class to Store Credentials with a ClientId & Secret pair
+	/// </summary>
+	public class TwitchApiCredentials
+	{
+		public string ClientId { get; set; }
+		public string ClientSecret { get; set; }
+	}
+
+	/// <summary>
+	/// Class to Store Credentials with a ClientId & Secret pair
+	/// </summary>
+	public class StreamLabsApiCedentials
+	{
+		public string ClientId { get; set; }
+		public string ClientSecret { get; set; }
+	}
+}
